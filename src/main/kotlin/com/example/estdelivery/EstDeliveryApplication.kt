3037c8efdac479b86cecc62d5712d3069a0b991d
ueibin <at365@naver.com>
2024-02-01 20:27:39 +0900

    setting : 세팅 진행

---
+++ src/main/kotlin/com/example/estdelivery/EstDeliveryApplication.kt
@@ -0,0 +1,11 @@
+package com.example.estdelivery
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EstDeliveryApplication
+
+fun main(args: Array<String>) {
+    runApplication<EstDeliveryApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/estdelivery/EstDeliveryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.estdelivery
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EstDeliveryApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
