01c526638eac238b84601767af34fe3ddb011ee8
이건창 <92219795+this-is-spear@users.noreply.github.com>
2024-02-04 23:09:14 +0900

    refactor : 쿠폰 종류 제한되도록 수정

--- src/main/kotlin/com/example/estdelivery/domain/coupon/Coupon.kt
+++ src/main/kotlin/com/example/estdelivery/domain/coupon/Coupon.kt
@@ -1,10 +1,23 @@
 package com.example.estdelivery.domain.coupon
 
-open class Coupon(
+sealed class Coupon(
     open val name: String,
     open val description: String,
     private val couponType: CouponType
 ) {
+    class RateDiscountCoupon(
+        val discountRate: Int,
+        override val name: String,
+        override val description: String,
+        couponType: CouponType
+    ) : Coupon(name, description, couponType)
+    class FixDiscountCoupon(
+        val discountAmount: Int,
+        override val name: String,
+        override val description: String,
+        couponType: CouponType
+    ) : Coupon(name, description, couponType)
+
     fun isPublished(): Boolean {
         return couponType == CouponType.IS_PUBLISHED
     }
