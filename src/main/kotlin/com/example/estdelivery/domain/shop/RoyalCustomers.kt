11b73af3094c17eac872dd18709fe3e8cfcf47ab
이건창 <92219795+this-is-spear@users.noreply.github.com>
2024-02-05 12:56:22 +0900

    refactor : royal customers 프로퍼티 immutable list 로 수정

--- src/main/kotlin/com/example/estdelivery/domain/shop/RoyalCustomers.kt
+++ src/main/kotlin/com/example/estdelivery/domain/shop/RoyalCustomers.kt
@@ -4,7 +4,7 @@ import com.example.estdelivery.domain.coupon.Coupon
 import com.example.estdelivery.domain.member.Member
 
 class RoyalCustomers(
-    private val customers: MutableList<Member> = mutableListOf()
+    private var customers: List<Member> = listOf()
 ) {
     fun handOutCoupon(coupon: Coupon) {
         customers.forEach { it.receiveCoupon(coupon) }
@@ -16,7 +16,7 @@ class RoyalCustomers(
                 throw IllegalArgumentException("이미 등록된 회원입니다.")
             }
         }
-        customers.addAll(members)
+        customers = customers + members
     }
 
     fun showRoyalCustomers(): List<Member> {
