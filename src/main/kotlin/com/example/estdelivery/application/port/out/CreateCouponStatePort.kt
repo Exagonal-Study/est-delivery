8fffaadc98d65f8bef72d22e8fd7963a10682a12
이건창 <92219795+this-is-spear@users.noreply.github.com>
2024-02-11 21:13:18 +0900

    refactor : port 중 클래스로 생성된 부분 인터페이스로 수정

--- src/main/kotlin/com/example/estdelivery/application/port/out/CreateCouponStatePort.kt
+++ src/main/kotlin/com/example/estdelivery/application/port/out/CreateCouponStatePort.kt
@@ -2,8 +2,6 @@ package com.example.estdelivery.application.port.out
 
 import com.example.estdelivery.domain.coupon.Coupon
 
-class CreateCouponStatePort {
-    fun create(coupon: Coupon) {
-        TODO("Not yet implemented")
-    }
+interface CreateCouponStatePort {
+    fun create(coupon: Coupon)
 }
--- src/main/kotlin/com/example/estdelivery/application/port/out/LoadShopOwnerStatePort.kt
+++ src/main/kotlin/com/example/estdelivery/application/port/out/LoadShopOwnerStatePort.kt
@@ -2,9 +2,6 @@ package com.example.estdelivery.application.port.out
 
 import com.example.estdelivery.application.port.out.state.ShopOwnerState
 
-class LoadShopOwnerStatePort {
-    fun findById(shopOwnerId: Long): ShopOwnerState {
-        TODO("Not yet implemented")
-    }
-
+interface LoadShopOwnerStatePort {
+    fun findById(shopOwnerId: Long): ShopOwnerState
 }
