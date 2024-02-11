854ae84f344704d24313bed20a92b7c3398611fd
이건창 <92219795+this-is-spear@users.noreply.github.com>
2024-02-11 15:19:49 +0900

    refactor : 임포트 문 정리

--- src/test/kotlin/com/example/estdelivery/domain/shop/HandOutCouponBookTest.kt
+++ src/test/kotlin/com/example/estdelivery/domain/shop/HandOutCouponBookTest.kt
@@ -1,12 +1,8 @@
 package com.example.estdelivery.domain.shop
 
-import com.example.estdelivery.domain.fixture.게시된_고정_할인_쿠폰
 import com.example.estdelivery.domain.fixture.나눠줄_쿠폰
-import com.example.estdelivery.domain.member.Member
-import com.example.estdelivery.domain.member.UnusedCouponBook
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FreeSpec
-import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
 class HandOutCouponBookTest : FreeSpec({
