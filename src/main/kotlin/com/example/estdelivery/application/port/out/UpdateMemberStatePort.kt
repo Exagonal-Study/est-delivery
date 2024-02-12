5c219b94a5f0c600081063e505a5a3f98cd1670e
이건창 <92219795+this-is-spear@users.noreply.github.com>
2024-02-12 13:14:01 +0900

    refactor : updateMember -> update 로 메서드명 수정

--- src/main/kotlin/com/example/estdelivery/application/port/out/UpdateMemberStatePort.kt
+++ src/main/kotlin/com/example/estdelivery/application/port/out/UpdateMemberStatePort.kt
@@ -3,5 +3,5 @@ package com.example.estdelivery.application.port.out
 import com.example.estdelivery.application.port.out.state.MemberState
 
 interface UpdateMemberStatePort {
-    fun updateMember(memberState: MemberState)
+    fun update(memberState: MemberState)
 }
