02fa566cbff48f717d50f59b7906ba58972e930d
subin0113 <subin0113@estsoft.com>
2024-02-12 19:14:06 +0900

    fix : require 문 오류 수정

--- src/main/kotlin/com/example/estdelivery/application/domain/model/CouponCode.kt
+++ src/main/kotlin/com/example/estdelivery/application/domain/model/CouponCode.kt
@@ -1,12 +1,13 @@
 package com.example.estdelivery.application.domain.model
 
+import com.example.estdelivery.common.Constant.COUPON_CODE_LENGTH
 import com.example.estdelivery.common.exception.CommonException
 import com.example.estdelivery.common.exception.ErrorCode
 
 @JvmInline
 value class CouponCode(private val value: String) {
     init {
-        require(value.length != 17) { throw CommonException(ErrorCode.INVALID_COUPON_CODE) }
+        require(value.length == COUPON_CODE_LENGTH) { throw CommonException(ErrorCode.INVALID_COUPON_CODE) }
     }
 
     fun get(): String = value
--- src/main/kotlin/com/example/estdelivery/common/Constant.kt
+++ src/main/kotlin/com/example/estdelivery/common/Constant.kt
@@ -3,4 +3,5 @@ package com.example.estdelivery.common
 object Constant {
     const val X_AUTHENTICATED_USER = "X-Authenticated-User"
     const val MAX_MACHINE_ID_LENGTH = 5
+    const val COUPON_CODE_LENGTH = 17
 }
