d861b5e86c661c31d63baf5e9ec21aaf4a52cc3c
이건창 <92219795+this-is-spear@users.noreply.github.com>
2024-02-12 14:10:10 +0900

    test : 의미 있는 이름을 가진 변수로 수정

--- src/test/kotlin/com/example/estdelivery/domain/fixture/ShopFixture.kt
+++ src/test/kotlin/com/example/estdelivery/domain/fixture/ShopFixture.kt
@@ -1,10 +1,11 @@
 package com.example.estdelivery.domain.fixture
 
+import com.example.estdelivery.domain.coupon.Coupon
 import com.example.estdelivery.domain.coupon.CouponBook
 import com.example.estdelivery.domain.shop.*
 
 const val 가게_이름 = "프리퍼"
-val 프리퍼 = Shop(
+val 새로_창업해서_아무것도_없는_프리퍼 = Shop(
     PublishedCouponBook(CouponBook(listOf())),
     HandOutCouponBook(CouponBook(listOf())),
     UsedCouponBook(CouponBook(listOf())),
@@ -13,7 +14,7 @@ val 프리퍼 = Shop(
     1L
 )
 val 단골_명단 = listOf(나눠준_쿠폰을_가진_삼건창, 일건창, 이건창)
-val 단골이_있는_프리퍼 = Shop(
+val 단골이_있는_멋진_프리퍼 = Shop(
     PublishedCouponBook(CouponBook(listOf())),
     HandOutCouponBook(CouponBook(listOf())),
     UsedCouponBook(CouponBook(listOf())),
@@ -29,3 +30,23 @@ val 나눠준_쿠폰이_있는_프리퍼 = Shop(
     가게_이름,
     3L
 )
+val 게시된_쿠폰이_있는_프리퍼 = Shop(
+    PublishedCouponBook(CouponBook(listOf(게시된_고정_할인_쿠폰))),
+    HandOutCouponBook(CouponBook(listOf())),
+    UsedCouponBook(CouponBook(listOf())),
+    RoyalCustomers(단골_명단),
+    가게_이름,
+    3L
+)
+
+fun 게시된_쿠폰이_있는_프리퍼(vararg coupon: Coupon): Shop {
+    return Shop(
+        PublishedCouponBook(CouponBook(listOf(*coupon))),
+        HandOutCouponBook(CouponBook(listOf())),
+        UsedCouponBook(CouponBook(listOf())),
+        RoyalCustomers(listOf()),
+        "프리퍼",
+        1L
+    )
+}
+
