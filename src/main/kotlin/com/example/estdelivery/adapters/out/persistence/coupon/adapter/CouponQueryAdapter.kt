a292c0c091e1c7802a7c19d37ecb6774aead57cd
sksk713 <sangk3758@gmail.com>
2024-02-13 01:12:33 +0900

    refactor: 쿠폰 조회 시, 만료일 지난 건 조회되지 않도록 수정

--- src/main/kotlin/com/example/estdelivery/adapters/out/persistence/coupon/adapter/CouponQueryAdapter.kt
+++ src/main/kotlin/com/example/estdelivery/adapters/out/persistence/coupon/adapter/CouponQueryAdapter.kt
@@ -6,6 +6,7 @@ import com.example.estdelivery.application.port.out.coupon.GetCouponsQueryPort
 import com.example.estdelivery.domain.coupon.Coupon
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDate
 
 @Component
 @Transactional(readOnly = true)
@@ -13,6 +14,7 @@ class CouponQueryAdapter(
     private val couponRepository: CouponRepository
 ) : GetCouponsQueryPort {
     override fun getCoupons(): List<Coupon> {
-        return couponRepository.findAll().map { CouponMapper.toDomain(it) }
+        val todayDate = LocalDate.now()
+        return couponRepository.findByExpiryDateAfter(todayDate).map { CouponMapper.toDomain(it) }
     }
 }
--- src/main/kotlin/com/example/estdelivery/adapters/out/persistence/coupon/respository/CouponRepository.kt
+++ src/main/kotlin/com/example/estdelivery/adapters/out/persistence/coupon/respository/CouponRepository.kt
@@ -2,6 +2,8 @@ package com.example.estdelivery.adapters.out.persistence.coupon.respository
 
 import com.example.estdelivery.adapters.out.persistence.coupon.entity.CouponEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import java.time.LocalDate
 
 interface CouponRepository : JpaRepository<CouponEntity, Long> {
+    fun findByExpiryDateAfter(currentDate: LocalDate): List<CouponEntity>
 }
