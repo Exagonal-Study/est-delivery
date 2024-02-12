d1fa02391514a63d064b58e8a0b49068659bad52
sksk713 <sangk3758@gmail.com>
2024-02-12 21:31:01 +0900

    feat: 사용자 생성 유스케이스 구현

---
+++ src/main/kotlin/com/example/estdelivery/application/service/MemberService.kt
@@ -0,0 +1,19 @@
+package com.example.estdelivery.application.service
+
+import com.example.estdelivery.adapters.`in`.web.dto.request.MemberCommand
+import com.example.estdelivery.adapters.`in`.web.dto.request.toCreateDomain
+import com.example.estdelivery.application.dto.member.MemberResponse
+import com.example.estdelivery.application.port.`in`.member.CreateMemberUseCase
+import com.example.estdelivery.application.port.out.member.MemberPersistencePort
+import org.springframework.stereotype.Service
+
+@Service
+class MemberService(
+    private val memberPersistencePort: MemberPersistencePort
+) : CreateMemberUseCase {
+    override fun createMember(request: MemberCommand): MemberResponse {
+        val member = request.toCreateDomain()
+        val savedMember = memberPersistencePort.save(member)
+        return MemberResponse(savedMember.name)
+    }
+}
