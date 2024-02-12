8570a2c674fc2de4cbdc76308f67f0fd92e20684
kimueibin <at365@naver.com>
2024-02-12 23:33:53 +0900

    refactor : 쿠폰 발급 object 클래스 내 함수만 제공할 수 있도록 수정

--- src/main/kotlin/com/example/estdelivery/domain/generator/CouponIdGenerator.kt
+++ src/main/kotlin/com/example/estdelivery/domain/generator/CouponIdGenerator.kt
@@ -3,10 +3,8 @@ package com.example.estdelivery.domain.generator
 import com.example.estdelivery.domain.coupon.CouponNumber
 import com.github.f4b6a3.tsid.TsidCreator
 
-object CouponIdGenerator {
+object CouponIdGenerator
 
-    fun generate(): CouponNumber = CouponNumber(
-        TsidCreator.getTsid().toString()
-    )
-
-}
+fun generate(): CouponNumber = CouponNumber(
+    TsidCreator.getTsid().toString()
+)
