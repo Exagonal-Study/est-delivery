814a929ecd40e0c2168520ef3cfae8d80816e24d
이건창 <92219795+this-is-spear@users.noreply.github.com>
2024-02-16 22:58:25 +0900

    refactor : 사용하지 않는 상태 제거

--- src/main/kotlin/com/example/estdelivery/application/port/out/UpdateShopStatePort.kt
+++
@@ -1,7 +0,0 @@
-package com.example.estdelivery.application.port.out
-
-import com.example.estdelivery.application.port.out.state.ShopState
-
-interface UpdateShopStatePort {
-    fun update(shopState: ShopState)
-}
